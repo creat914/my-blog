8da5a280879829b3698daa95afb4374f163580a1
miancan <1011148498@qq.com>
2022-08-31 17:00:27 +0800

    重新构建

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -104,7 +104,7 @@ module.exports = {
             "/blogs/utils/":[
                 "npmPage",
                 "developmentOfNotes"
-            ]
+            ],
         },
         "type": "blog",
         "blogConfig": {
